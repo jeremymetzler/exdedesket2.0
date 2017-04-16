82711c51bdf769317a45a7d0e991317b77b44dd7
jeremymetzler <jeremystickrpg@gmail.com>
2017-04-16 02:15:58 -0400

    Update Resolver.cpp

--- Resolver.cpp
+++ Resolver.cpp
@@ -36,21 +36,46 @@ void R::Resolve()
 			if (bLowerBodyIsUpdated) StoredYaw = pEntity->GetLowerBodyYaw();
 			if (pEntity->GetFlags() & FL_ONGROUND && pEntity->GetVelocity().Length2D() != 0) eyeAngles->y = pEntity->GetLowerBodyYaw();
 			else
+			{
+				if (Menu::Window.RageBotTab.AccuracyResolver.GetIndex() == 1 || Menu::Window.RageBotTab.AccuracyResolver.GetIndex() == 2)
+				{
+					int num = Globals::Shots % 5;
+					switch (num) {
+					case 0:eyeAngles->y = AimAngle.y - 0; break;
+					case 1:eyeAngles->y = AimAngle.y - 15; break;
+					case 2:eyeAngles->y = AimAngle.y + 40; break;
+					case 3:eyeAngles->y = AimAngle.y - 40; break;
+					case 4:eyeAngles->y = AimAngle.y + 15; break;
+					}
+				}
+			/*	if (Menu::Window.RageBotTab.AccuracyAutoResolver.GetIndex() == 4 && eyeAngles->y != pEntity->GetLowerBodyYaw())
+				{
+					int num = Globals::Shots % 9;
+					switch (num) {
+					case 0:eyeAngles->y = AimAngle.y - 0; break;
+					case 1:eyeAngles->y = AimAngle.y + 45; break;
+					case 2:eyeAngles->y = AimAngle.y + 90; break;
+					case 3:eyeAngles->y = AimAngle.y + 135; break;
+					case 4:eyeAngles->y = AimAngle.y + 180; break;
+					case 5:eyeAngles->y = AimAngle.y + 225; break;
+					case 6:eyeAngles->y = AimAngle.y + 270; break;
+					case 7:eyeAngles->y = AimAngle.y + 315; break;
+					case 8:eyeAngles->y = AimAngle.y + 360; break;
+					}
+				} */
+			}
 
-			if (Menu::Window.RageBotTab.AccuracyResolver.GetIndex() == 1 || Menu::Window.RageBotTab.AccuracyResolver.GetIndex() == 2)
-			Vector AimAngle;
-			CalcAngleYawOnly(pEntity->GetEyePosition(), pLocal->GetAbsOrigin2(), AimAngle);
-			NormalizeVector(AimAngle);
-			static float StoredYaw = 0;
-			static bool bLowerBodyIsUpdated = false;
-			if (pEntity->GetLowerBodyYaw() != StoredYaw) bLowerBodyIsUpdated = true;
-			else bLowerBodyIsUpdated = false;
-			if (bLowerBodyIsUpdated) StoredYaw = pEntity->GetLowerBodyYaw();
-			if (pEntity->GetFlags() & FL_ONGROUND && pEntity->GetVelocity().Length2D() != 0) eyeAngles->y = pEntity->GetLowerBodyYaw();
-			
-	        
-		   	}
+			if (Menu::Window.RageBotTab.AccuracyResolver.GetIndex() == 2) //pitch
+			{
+				std::string strPitch = std::to_string(eyeAngles->x);
+
+				if (eyeAngles->x < -179.f) eyeAngles->x += 360.f;
+				else if (eyeAngles->x > 90.0 || eyeAngles->x < -90.0) eyeAngles->x = 89.f;
+				else if (eyeAngles->x > 89.0 && eyeAngles->x < 91.0) eyeAngles->x -= 90.f;
+				else if (eyeAngles->x > 179.0 && eyeAngles->x < 181.0) eyeAngles->x -= 180;
+				else if (eyeAngles->x > -179.0 && eyeAngles->x < -181.0) eyeAngles->x += 180;
+				else if (fabs(eyeAngles->x) == 0) eyeAngles->x = std::copysign(89.0f, eyeAngles->x);
+			}
 		}
-
 	}
-
+}
